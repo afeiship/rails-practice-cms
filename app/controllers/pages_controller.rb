e718af6955fddd56a42eb96888c5d555347dc0b3
afeiship <1290657123@qq.com>
2017-05-05 14:46:29 +0800

    ""[A

--- app/controllers/pages_controller.rb
+++
@@ -1,8 +0,0 @@
-class PagesController < ApplicationController
-    pages = %w[ page1 page2 page3 ]
-    pages.each do |page|
-        define_method(page) do
-            #do nothing
-        end
-    end
-end
