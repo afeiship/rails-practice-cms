9228ea682ba63a7d8edf53828d459902b4b739d6
afeiship <1290657123@qq.com>
2017-05-16 22:48:30 +0800

    add root page

---
+++ config/initializers/rails_env_admin.rb
@@ -0,0 +1,4 @@
+# You can export this fields to bash env file:
+
+ENV['ADMIN_USER'] = "afei"
+ENV['ADMIN_PASSWORD'] = "abc"
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  # mount RailsPages::Engine => "/blog"
-  # get 'pages/page2';
-  # get 'pages/page3';
-
-
+  root :to => 'pages#page1'
+  get '/page1', to: 'pages#page1'
 
 end
