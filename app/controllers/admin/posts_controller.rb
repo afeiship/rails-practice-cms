d3babe7abe25b4f5374c348bad696a7ab5bbbb2f
afeiship <1290657123@qq.com>
2017-07-01 18:55:50 +0800

    issue:show index

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,57 @@
-class Admin::PostsController < Admin::CurdController
-  
-  model Admin::Post
+class Admin::PostsController < Admin::BaseController
+  def index
+    @admin_posts = Admin::Post.all
+  end
 
+  def show
+    @admin_post = Admin::Post.find(params[:id])
+  end
+
+  def edit
+    @admin_post = Admin::Post.find(params[:id])
+  end
+
+
+  def new
+    @admin_post = Admin::Post.new
+  end
+
+  def create
+    @admin_post = Admin::Post.new(admin_post_params)
+    if @admin_post.save
+      redirect_to @admin_post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @admin_post = Admin::Post.find(params[:id])
+    if @admin_post.update(admin_post_params)
+      redirect_to @admin_post, notice: 'Post was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @admin_post = Admin::Post.find(params[:id])
+    @admin_post.destroy
+    redirect_to admin_posts_url, notice: 'Post was successfully destroyed.'
+  end
+
+
+  private
+  def set_admin_post
+    @admin_post = Admin::Post.find(params[:id])
+  end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def admin_post_params
+    if params[:action] == 'new'
+      nil
+    else
+      params.require(:admin_post).permit(:title, :content)
+    end
+  end
 
 end
